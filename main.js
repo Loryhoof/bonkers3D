4dfe04a9efd091f70c9b780595060c1b12fea835
Kevin <klattkev@gmail.com>
2024-03-25 11:04:32 +0100

    dasasd

--- main.js
+++ main.js
@@ -30,32 +30,32 @@ const grass_step_sound = new THREE.Audio(listener);
 
 const audioLoader = new THREE.AudioLoader();
 
-audioLoader.load( 'audio/pistol_shot_2.mp3', function( buffer ) {
+audioLoader.load( '/audio/pistol_shot_2.mp3', function( buffer ) {
 	pistol_shoot_sound.setBuffer( buffer );
 	pistol_shoot_sound.setLoop( false );
 	pistol_shoot_sound.setVolume( 0.3 );
 });
 
-audioLoader.load( 'audio/ambience.mp3', function( buffer ) {
+audioLoader.load( '/audio/ambience.mp3', function( buffer ) {
 	ambience_sound.setBuffer( buffer );
 	ambience_sound.setLoop( true );
 	ambience_sound.setVolume( 0.225 );
     ambience_sound.play()
 });
 
-audioLoader.load( 'audio/reload.mp3', function( buffer ) {
+audioLoader.load( '/audio/reload.mp3', function( buffer ) {
 	pistol_reload_sound.setBuffer( buffer );
 	pistol_reload_sound.setLoop( false );
 	pistol_reload_sound.setVolume( 0.75 );
 });
 
-audioLoader.load( 'audio/impact2.mp3', function( buffer ) {
+audioLoader.load( '/audio/impact2.mp3', function( buffer ) {
 	bullet_impact_sound.setBuffer( buffer );
 	bullet_impact_sound.setLoop( false );
 	bullet_impact_sound.setVolume( 0.4 );
 });
 
-audioLoader.load( 'audio/grass_step2.mp3', function( buffer ) {
+audioLoader.load( '/audio/grass_step2.mp3', function( buffer ) {
 	grass_step_sound.setBuffer( buffer );
 	grass_step_sound.setLoop( false );
 	grass_step_sound.setVolume( 0.2 );
